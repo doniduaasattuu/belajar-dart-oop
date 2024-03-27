356a01b64737a3ef6236c39fe4ad9707b3e7fc44
doniduaasattuu <doni.duaasattuu@gmail.com>
2024-03-27 15:21:47 +0700

    setup belajar-dart-oop class

---
+++ bin/class.dart
@@ -0,0 +1,22 @@
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = 'Indonesia';
+
+  void sayHello(String paramName) {
+    print('Hello $paramName, my name is $name');
+  }
+}
+
+void main(List<String> args) {
+  var nama = Person();
+
+  print(nama.name);
+  nama.name = 'Doni';
+  print(nama.address);
+  nama.address = 'Cikarang';
+  print(nama.address);
+  print(nama.country);
+
+  nama.sayHello('Budi'); // Hello Budi, my name is Doni
+}
